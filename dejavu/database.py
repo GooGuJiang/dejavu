8eb38008486caae4b31799f968bdb9d15bab8b42
咕谷酱 <74496778+GooGuJiang@users.noreply.github.com>
2022-08-01 19:09:11 +0800

    修复 Binary 报错问题

--- dejavu/database.py
+++ dejavu/database.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import
 import binascii
-from tenacity import retry, wait_fixed, stop_after_attempt
 
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Binary, ForeignKey, UniqueConstraint, Index
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_utils import database_exists
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import LargeBinary
 Base = declarative_base()
 
 
@@ -16,34 +14,30 @@ class Song(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(length=255), nullable=False)
     fingerprinted = Column(Boolean, default=False)
-    file_sha1 = Column(String(length=40), nullable=False)
-    
-    ix_sha1 = Index('ix_sha', file_sha1)
+    file_sha1 = Column(LargeBinary(length=20), nullable=False)
 
 
 class Fingerprint(Base):
     __tablename__ = "fingerprints"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    hash = Column(String(length=16), nullable=False)
+    hash = Column(LargeBinary(length=10), nullable=False)
     song_id = Column(
         Integer, ForeignKey(Song.id, ondelete="CASCADE"), nullable=False
     )
     offset = Column(Integer, nullable=False)
 
     unique = UniqueConstraint('hash', 'song_id', 'offset')
-    ix_hash = Index('ix_hash', hash, mysql_length=8)  # avoid total duplication of value
 
 
 class Database(object):
     def __init__(self, url):
         super(Database, self).__init__()
         self.url = url
-        self.engine = create_engine(url, pool_pre_ping=True)
+        self.engine = create_engine(url)
         self.session = sessionmaker(bind=self.engine)()
-        
-        self.__is_db_ready__(self.url)
         Base.metadata.create_all(self.engine)
+
         # clean by deleting not fully fingerprinted songs; possibly because of abruptly killed previous run
         self.session.query(Song).filter(Song.fingerprinted.is_(False)).delete()
         self.session.commit()
@@ -58,18 +52,9 @@ class Database(object):
         song.fingerprinted = True
         self.session.commit()
 
-    # def get_songs(self): - don't cache all of this!
-
-    def get_song_by_hash(self, song_hash):
-        """
-        Return a song by its hash
-
-        :param song_hash: Song hash
-        """
-        song = self.session.query(Song).filter(Song.file_sha1 == binascii.b2a_base64(binascii.unhexlify(song_hash))).one_or_none()
-        if song is None: 
-            return None
-        return song.id
+    def get_songs(self):
+        """Returns all fully fingerprinted songs in the database."""
+        return self.session.query(Song).filter(Song.fingerprinted)
 
     def get_song_by_id(self, sid):
         """
@@ -87,7 +72,7 @@ class Database(object):
         :param song_name: name of the song
         :param file_hash: sha1 hex digest of the filename
         """
-        song = Song(name=song_name, file_sha1=binascii.b2a_base64(binascii.unhexlify(file_hash)))
+        song = Song(name=song_name, file_sha1=binascii.unhexlify(file_hash))
         self.session.add(song)
         self.session.commit()
         return song.id
@@ -105,7 +90,7 @@ class Database(object):
         for hash, offset in set(hashes):
             fingerprints.append(
                 Fingerprint(
-                    hash=binascii.b2a_base64(binascii.unhexlify(hash)),
+                    hash=binascii.unhexlify(hash),
                     song_id=sid,
                     offset=int(offset)
                 )
@@ -128,19 +113,13 @@ class Database(object):
         # Create a dictionary of hash => offset pairs for later lookups
         mapper = {}
         for hash, offset in hashes:
-            mapper[binascii.b2a_base64(binascii.unhexlify(hash)) ] = offset
+            mapper[hash.upper()] = offset
 
         # Get an iterable of all the hashes we need
-        values = [h for h in mapper.keys()]
-        
-        for i in range(0, len(values), 500):
-            k = i + 500
-            tmp = values[i:k]
-            for fingerprint in self.session.query(Fingerprint).filter(
-                    Fingerprint.hash.in_(tmp)
-                    ):
-                yield (fingerprint.song_id, fingerprint.offset - mapper[fingerprint.hash])
+        values = [binascii.unhexlify(h) for h in mapper.keys()]
 
-    @retry(wait=wait_fixed(1),stop=stop_after_attempt(10))
-    def __is_db_ready__(self, url):
-        database_exists(url)
+        for fingerprint in self.session.query(Fingerprint).filter(
+            Fingerprint.hash.in_(values)
+        ):
+            hash = binascii.hexlify(fingerprint.hash).upper().decode('utf-8')
+            yield (fingerprint.song_id, fingerprint.offset - mapper[hash])
